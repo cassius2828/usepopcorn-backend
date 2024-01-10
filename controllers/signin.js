3b930ab7158e204e2890e51b36992d77ccdcc872
Cassius Reynolds <cassius2828@gmail.com>
2024-01-10 14:33:12 -0800

    improved readablity and optimized login method by creating a loginMethod function instead of duplicating large amounts of code

--- controllers/signin.js
+++ controllers/signin.js
@@ -3,42 +3,20 @@ const handleSignIn = (req, res, bcrypt, db) => {
   if ((!email && !username) || !password) {
     return res.status(400).json("incorrect submission");
   }
-  if (!username) {
-    db.select("email", "hash")
-      .from("login")
-      .where("email", "=", email)
-      .then((data) => {
-        const isValid = true
-        // bcrypt.compareSync(password, data[0].hash);
 
-        if (isValid) {
-         
-          return db
-            .select("*")
-            .from("users")
-            .where("email", "=", email)
-            .then((user) => {
-              res.json(user[0]);
-            })
-            .catch((err) => res.send("unable to get user"));
-        } else {
-          res.status(400).json("wrong credentials entered");
-        }
-      });
-  } 
-  else if (!email) {
-    db.select("username", "hash")
+// * allows use of function and args to clean up code and improve readability
+  const loginMethod = (methodStr, method) => {
+    db.select(methodStr, "hash")
       .from("login")
-      .where("username", "=", username)
+      .where(methodStr, "=", method)
       .then((data) => {
         const isValid = bcrypt.compareSync(password, data[0].hash);
 
         if (isValid) {
-        
           return db
             .select("*")
             .from("users")
-            .where("username", "=", username)
+            .where(methodStr, "=", method)
             .then((user) => {
               res.json(user[0]);
             })
@@ -47,6 +25,12 @@ const handleSignIn = (req, res, bcrypt, db) => {
           res.status(400).json("wrong credentials entered");
         }
       });
+  };
+
+  if (!username) {
+    loginMethod("email", email);
+  } else if (!email) {
+    loginMethod("username", username);
   }
 };
 

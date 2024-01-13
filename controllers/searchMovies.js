99464be6b6b27d618041e36e18fc7492500398bc
Cassius Reynolds <cassius2828@gmail.com>
2024-01-13 15:50:18 -0800

    removing node modules then applying it to gitignore

--- controllers/searchMovies.js
+++ controllers/searchMovies.js
@@ -1,13 +1,12 @@
 const fetchData = async (req, res) => {
   const { query } = req.body;
-  console.log(req.body);
   const response = await fetch(
     `http://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=${query}`
   );
 
   const data = await response.json();
   res.json(data);
-  if (data.Response === "False") throw new Error("Movie not found");
+//   if (data.Response === "False") throw new Error("Movie not found");
 };
 
 export default fetchData;
